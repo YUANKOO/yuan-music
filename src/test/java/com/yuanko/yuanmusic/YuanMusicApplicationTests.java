e4f8273074164121b09b27313382739dbe648cc1
yuanko <759790635@qq.com>
2022-05-15 01:05:10 +0800

    first commit

---
+++ src/test/java/com/yuanko/yuanmusic/YuanMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanko.yuanmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuanMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
